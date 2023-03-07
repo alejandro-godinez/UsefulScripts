3f0e7e0ef5e051b35a1dcae3ad549cb53fc66514
Alejandro Godinez <programmergodinez@gmail.com>
2023-03-07 09:38:37 -0600

    Added dedicated method to process arguments in various git scripts and the basics arguments

--- git/gitStashList.sh
+++ git/gitStashList.sh
@@ -3,7 +3,7 @@
 #  This script will list the stash entries of each of the git project folders 
 #  in the current directory.
 #  
-#  version: 2023.2.2
+#  version: 2023.3.7
 #
 #  TODO:
 #  - Add maxdepth variable and argument to control depth of search
@@ -42,6 +42,52 @@ function log {
   if [ "$DEBUG" = true ]; then 
     echo "$1"
   fi
+}
+
+#//process the arguments for the script
+function processArgs {
+  log "Arg Count: $#"
+  while (( $# > 0 )); do
+    arg=$1
+    
+    #//check for verbose
+    if [ "${arg^^}" = "-V" ]; then
+      DEBUG=true
+    fi
+    
+    log "Arg Count: $#"
+    log "Argument: ${arg^^}"
+
+    #//check for help
+    if [ "${arg^^}" = "-H" ]; then
+      printHelp
+      exit 0
+    fi
+    
+    #//check for depth
+    if [ "${arg^^}" = "-D" ]; then
+    
+      #//check if there are still more arguments where the number could be provided
+      if (( $# > 1 )); then
+        #//check the depth number from next argument
+        numValue=$2
+        log "  Depth Value: $numValue"
+        
+        if [[ $numValue =~ $RGX_NUM ]]; then
+          MAX_DEPTH=$numValue
+          log "  Max Depth: $MAX_DEPTH"
+          
+          #//shift number argument so it is not processed on next iteration
+          shift
+        fi
+      else
+        log "  No more arguments for number to exist"
+      fi
+    fi
+    
+    #//shift to next argument
+    shift
+  done
 }
 
 #// check if a directory is a git working directory
@@ -93,48 +139,7 @@ function printStashList {
 #-------------------------------
 
 #//check the command arguments
-log "Arg Count: $#"
-while (( $# > 0 )); do
-  arg=$1
-  
-  #//check for verbose
-  if [ "${arg^^}" = "-V" ]; then
-    DEBUG=true
-  fi
-  
-  log "Arg Count: $#"
-  log "Argument: ${arg^^}"
-
-  #//check for help
-  if [ "${arg^^}" = "-H" ]; then
-    printHelp
-    exit 0
-  fi
-  
-  #//check for depth
-  if [ "${arg^^}" = "-D" ]; then
-  
-    #//check if there are still more arguments where the number could be provided
-    if (( $# > 1 )); then
-      #//check the depth number from next argument
-      numValue=$2
-      log "  Depth Value: $numValue"
-      
-      if [[ $numValue =~ $RGX_NUM ]]; then
-        MAX_DEPTH=$numValue
-        log "  Max Depth: $MAX_DEPTH"
-        
-        #//shift number argument so it is not processed on next iteration
-        shift
-      fi
-    else
-      log "  No more arguments for number to exist"
-    fi
-  fi
-  
-  #//shift to next argument
-  shift
-done
+processArgs "$@"
 
 #//identify if current directory is a git project directory
 currDir=$(pwd)
