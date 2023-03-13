a6a4fae36826cacfe5b1b5d9c0c29ad83eb9ca9a
Alejandro Godinez <programmergodinez@gmail.com>
2023-03-13 10:22:53 -0500

    git_lib.sh - added gitPull method gitPullMain.sh - modified to import logging and git lib scripts and user

--- git/gitPullMain.sh
+++ git/gitPullMain.sh
@@ -12,14 +12,18 @@
 set -u #//error on unset variable
 set -e #//exit on error
 
+#//import logging functionality
+source ~/lib/logging.sh
+
+#//import git functionality
+source ~/lib/git_lib.sh
+
 #//set the Internal Field Separator to newline (git-bash uses spaces for some reason)
 IFS=$'\n'
 
 GRN='\033[0;32m'
 YEL='\033[1;33m'
 NC='\033[0m' # No Color
-
-DEBUG=false #//toggle debug output
 
 #//prompt to perform pull on non main branches
 PULL_ALL=FALSE
@@ -42,12 +46,6 @@ function printHelp {
   echo "    -v      Verbose/debug output"
   echo "    -a      Prompt to pull for all ALL branches"
   echo "    -d num    Search depth (default 1)"
-}
-
-function log {
-  if [ "$DEBUG" = true ]; then
-    echo "$1"
-  fi
 }
 
 #//process the arguments for the script
@@ -126,23 +124,11 @@ function waitForInput {
   fi
 }
 
-#// check if a directory is a git working directory
-function isGitDir {
-  local theDir=$1
-  log "  The Dir: ${theDir}"
-  if [ -d "${theDir}" ] && [ -d "${theDir}/.git" ]; then
-    log "  Is Git Directory: TRUE"
-    return 0
-  fi
-  log "  Is Git Directory: FALSE"
-  return 1
-}
-
-function gitPull {
+function gitPullMain {
   local repoDir=$1
 
   #//get current working branch
-  branch=$(git -C $repoDir rev-parse --abbrev-ref HEAD)
+  branch=$(gitBranchName $repoDir)
   log "  Branch: ${branch}"
 
   #//skip if branch is not the main branch
@@ -161,7 +147,7 @@ function gitPull {
 
   #//perform the git pull
   log "  Performing the pull..."
-  git -C $repoDir pull
+  gitPull $repoDir
 }
 
 #-----------------------------
@@ -179,7 +165,7 @@ log "Checking current directory..."
 if isGitDir "${currDir}"; then
   #// pull main branch
   log "  Performing the pull..."
-  gitPull "${currDir}"
+  gitPullMain "${currDir}"
 
   exit 0
 else
@@ -199,7 +185,7 @@ do
   fi
 
   #// pull main branch
-  gitPull "${aDir}"
+  gitPullMain "${aDir}"
 
 done
 log "DONE"
